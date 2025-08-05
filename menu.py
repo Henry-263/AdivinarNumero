2ec99e5b00a62d5da339c5311350de7220f3bd4f
Henry <henrysanchezjuan@gmail.com>
2025-08-06 01:08:39 +0200

    Ahora cuando ganas o pierdes el color del texto cambia

--- menu.py
+++ menu.py
@@ -16,14 +16,17 @@ ganar = False
 active = True
 texto = ["Dale enter para jugar"]
 listoParaJugar = True
-
+color = [0, 0, 0]
 
 while active:
 
     screen.blit(recursos.fondo, (0, 0))
 
     for i, linea in enumerate(texto):
-        texto_menu = recursos.font_grande.render(linea, True, (0, 0, 0))
+        if ganar:
+            texto_menu = recursos.font_grande.render(linea, True, (color[0], color[1], color[2]))
+        else:
+            texto_menu = recursos.font_grande.render(linea, True, (color[0], color[1], color[2]))
         screen.blit(texto_menu, (recursos.anchura // 2 - texto_menu.get_width() // 2, 200 + i * 40))
 
 
@@ -40,12 +43,15 @@ while active:
                     active = False
                 elif ganar:
                     texto = ["Felicidades has ganado"]
+                    color = [0, 143, 57]
                 else:
                     texto = ["Te has quedado sin intentos,", "has perdido"]
+                    color = (255, 0, 0)
                 inicio_tiempo = pygame.time.get_ticks()
 
     if pygame.time.get_ticks() - inicio_tiempo > 3000:
         texto = ["Dale enter para jugar"]
+        color = [0, 0, 0]
         listoParaJugar = True
 
 
